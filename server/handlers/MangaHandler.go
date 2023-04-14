e891535c4dd9f32deaa3e96c95dbb379d3527eaf
vinicius.moraes <vimoraescarvalho@gmail.com>
2023-04-13 21:28:52 -0300

    headers

--- server/handlers/MangaHandler.go
+++ server/handlers/MangaHandler.go
@@ -408,8 +408,20 @@ func (m *MangaHandler) TestaConexao() error {
 	if err != nil {
 		return errors.New(fmt.Sprintf("Erro ao criar request URL: %s Error: %s", url, err.Error()))
 	}
-	req.Header.Set("User-Agent", "Mozilla/5.0 (iPad; CPU OS 12_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148")
-
+	req.Header.Set("Connection", `keep-alive`)
+	req.Header.Set("Cache-Control", `max-age=0`)
+	req.Header.Set("sec-ch-ua", `" Not A;Brand";v="99", "Chromium";v="99", "Google Chrome";v="99"`)
+	req.Header.Set("sec-ch-ua-mobile", `?0`)
+	req.Header.Set("sec-ch-ua-platform", `macOS`)
+	req.Header.Set("Upgrade-Insecure-Requests", `1`)
+	req.Header.Set("User-Agent", `Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.83 Safari/537.36`)
+	req.Header.Set("Accept", `text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9`)
+	req.Header.Set("Sec-Fetch-Site", `none`)
+	req.Header.Set("Sec-Fetch-Mode", `navigate`)
+	req.Header.Set("Sec-Fetch-User", `?1`)
+	req.Header.Set("Sec-Fetch-Dest", `document`)
+	req.Header.Set("Accept-Encoding", `gzip, deflate, br`)
+	req.Header.Set("Accept-Language", `en-GB,en-US;q=0.9,en;q=0.8`)
 	res, err := client.Do(req)
 	if err != nil {
 		return errors.New(fmt.Sprintf("Erro ao request URL: %s Error: %s", url, err.Error()))
